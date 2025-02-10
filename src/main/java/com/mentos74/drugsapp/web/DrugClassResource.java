d0d61e5c85f7d2b37e24f650f458e8d8f4a08c7d
mentos74 <rezza.sr@gmail.com>
2025-02-10 21:40:12 +0700

    additional drug class add dan list

--- src/main/java/com/mentos74/drugsapp/web/DrugClassResource.java
+++ src/main/java/com/mentos74/drugsapp/web/DrugClassResource.java
@@ -1,11 +1,16 @@
 package com.mentos74.drugsapp.web;
 
+import com.mentos74.drugsapp.dto.DrugClassCreateRequestDTO;
 import com.mentos74.drugsapp.dto.DrugClassResponseRequestDTO;
 import com.mentos74.drugsapp.service.DrugClassSevice;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -16,13 +21,35 @@ public class DrugClassResource {
     DrugClassSevice drugClassSevice;
 
 
-
     @GetMapping("/drug-class/list")
-    public String listDrugClass(Model model){
+    public String listDrugClass(Model model) {
 
-        List <DrugClassResponseRequestDTO> listDrugClass = drugClassSevice.listDrugClass();
-        model.addAttribute("dto", listDrugClass);
+        List<DrugClassResponseRequestDTO> listDrugClass = drugClassSevice.listDrugClass();
+        model.addAttribute("dtos", listDrugClass);
 
         return "/drug_class/list_drugClass";
     }
+
+    @GetMapping("drug-class/add")
+    public String addDrugClass(Model model) {
+        DrugClassCreateRequestDTO dto = new DrugClassCreateRequestDTO();
+        model.addAttribute("dto", dto);
+        return "/drug_class/add_drugClass";
+    }
+
+    @PostMapping("drug-class/add")
+    public String addDrugClassNew(@ModelAttribute("dto") @Valid DrugClassCreateRequestDTO dto,
+                                  BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("dto", dto);
+            return "/drug_class/add_drugClass";
+        }
+
+        drugClassSevice.createDrugClass(dto);
+
+        return "redirect:/drug-class/list";
+    }
+
+
 }
